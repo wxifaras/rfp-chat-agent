9c385cec2d009059f3437fdb8e45c524b5b1441a
William Xifaras <wxifaras@outlook.com>
2025-06-16 12:46:52 -0400

    model update

--- rfp-chat-agent-api/models/decision_log.py
+++ rfp-chat-agent-api/models/decision_log.py
@@ -1,31 +1,32 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class DecisionLog(BaseModel):
-    Pursuit_Name: str
-    Decision_by_CDD: str
+    #Pursuit_Name: Optional[str] = None
+    Decision_by_CDD: Optional[str] = None
     Details: str
     Est_TCV: str
     Division: str
     Pursuit_Sponsor: str
     CDD_Booking_Date: str
-    Link_to_pursuit_files: str
-    Associated_files: str
+    Link_to_pursuit_files: Optional[str] = None
+    Associated_files: Optional[str] = None
     Resources_Required: str
-    Contract_Term: str
+    Contract_Term: Optional[str] = None
     Pursuit_Due_Date: str
-    Has_the_Opportunity_Review_Policy_been_reviewed: str
+    Has_the_Opportunity_Review_Policy_been_reviewed: Optional[str] = None
     Client_Ministry_Name: str
-    Sign_off_status: str
-    Next_Step: str
-    Partner_Required: str
+    Sign_off_status: Optional[str] = None
+    Next_Step: Optional[str] = None
+    Partner_Required: Optional[str] = None
     I_have_reviewed_the_Opportunity_Review_Policy_the_pursuit_lead_has_the_following_triggers: str
     Deal_type_current: str
     Expected_Gross_Margin: str
     Does_Maximus_Canada_have_the_qualifications: str
-    Conditions: str
-    Pursuit_Lead: str
-    Salesforce_Opportunity_URL: str
-    Modified: str
-    Created: str
-    Created_By: str
-    Modified_By: str
+    Conditions: Optional[str] = None
+    Pursuit_Lead: Optional[str] = None
+    Salesforce_Opportunity_URL: Optional[str] = None
+    Modified: Optional[str] = None
+    Created: Optional[str] = None
+    Created_By: Optional[str] = None
+    Modified_By: Optional[str] = None
